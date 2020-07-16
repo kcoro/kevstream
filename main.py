35b871b8e04b180c287df58a583a04f46350891e
Kevin Coronato <kevincoronato@users.noreply.github.com>
2020-07-16 06:23:30 -0700

    Added api/v1 route

--- main.py
+++ main.py
@@ -4,8 +4,31 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
+# Index page will default to login page and validate user credentials.
+# Successful authentication will reroute user to homepage.
 @app.route('/')
 def homepage():
 	return '<h1>HOMEPAGE</h1>'
+
+# API v1 route
+@app.route('/api/v1')
+def apiv1():
+	return jsonify(sensorData)
+
+# Fake sensor data for API testing
+sensorData = [
+	       {
+		 'name': 'indoorSensor',
+		 'id': 0,
+		 'temperature': 76,
+		 'humiditiy': 52
+	       },
+	       {
+		 'name': 'outdoorSensor',
+		 'id': 1,
+		 'temperature': 88,
+		 'humidity': 85
+	       }
+	     ]
 
 
